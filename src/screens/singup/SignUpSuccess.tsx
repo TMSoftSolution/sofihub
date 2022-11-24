6fb64d700404377a4c075f6157385d069af3ddec
Full Stack Software Developer & Web & Mobile & Blockchain <lovetallman426@gmail.com>
2022-11-23 21:09:11 -0800

    modify sign up success

--- src/screens/singup/SignUpSuccess.tsx
+++ src/screens/singup/SignUpSuccess.tsx
@@ -2,14 +2,27 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import {RootStackParamList} from '../../App';
-import {Images} from '../../common';
+import {Colors, Fonts, Images, window} from '../../common';
+import {PrimaryButton, Spacer} from '../../components';
 
-type SignUpSuccessProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
+type SignUpSuccessProps = NativeStackScreenProps<
+  RootStackParamList,
+  'SignUpSuccess'
+>;
+
+const height = (443 * window.width) / 375.0;
 
 export const SignUpSuccess = ({navigation}: SignUpSuccessProps) => {
   return (
     <View style={styles.container}>
       <Image source={Images.imgSignUpSuccess} style={styles.image}></Image>
+      <Spacer space={16} />
+      <Text style={styles.title}>Welcome Rahi!</Text>
+      <Spacer space={8} />
+      <Text style={styles.subTitle}>Let's get you set up</Text>
+      <View style={styles.bottomContainer}>
+        <PrimaryButton text="NEXT" onClick={() => {}} />
+      </View>
     </View>
   );
 };
@@ -20,7 +33,27 @@ const styles = StyleSheet.create({
   },
   image: {
     width: '100%',
-    resizeMode: 'contain',
+    height: height,
+    resizeMode: 'cover',
     alignSelf: 'center',
+  },
+  title: {
+    color: Colors.primaryText,
+    fontFamily: Fonts.SFBold,
+    fontSize: 20,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  subTitle: {
+    color: Colors.secondaryText,
+    fontFamily: Fonts.SFRegular,
+    fontSize: 16,
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  bottomContainer: {
+    paddingHorizontal: 16,
+    marginTop: 64,
+    justifyContent: 'flex-end',
   },
 });
